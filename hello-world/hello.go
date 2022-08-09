5a860976322dc8c6cae4ec6bcc213cba1bb7f349
manujagobind <manujagobind@gmail.com>
2022-08-09 20:10:06 +0530

    Use switch instead of if

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -13,13 +13,17 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
 	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+
+	return prefix + name
 }
 
 func main() {

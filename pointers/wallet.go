032297b96a5b5c159ab284ae8f21ba4fafb4aadd
manujagobind <manujagobind@gmail.com>
2022-08-10 22:34:32 +0530

    Implement stringer

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,6 +1,16 @@
 package pointers
 
+import "fmt"
+
 type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 type Wallet struct {
 	balance Bitcoin

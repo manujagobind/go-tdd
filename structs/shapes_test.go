081ff5fc770495f2221f6417d898b356cf3744d6
manujagobind <manujagobind@gmail.com>
2022-08-10 22:03:35 +0530

    Use table driven tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -14,22 +14,18 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("got %.2f, want %.2f", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tc := range areaTests {
+		got := tc.shape.Area()
+		if got != tc.want {
+			t.Errorf("got %.2f, want %.2f", got, tc.want)
+		}
+	}
 }

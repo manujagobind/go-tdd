217d67054999d156db20b47b7c38bf2f2601a014
manujagobind <manujagobind@gmail.com>
2022-08-10 22:14:16 +0530

    Make test output more helpful

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,18 +15,21 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tc := range areaTests {
-		got := tc.shape.Area()
-		if got != tc.want {
-			t.Errorf("%#v got %.2f, want %.2f", tc.shape, got, tc.want)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.shape.Area()
+			if got != tc.hasArea {
+				t.Errorf("%#v got %.2f, want %.2f", tc.shape, got, tc.hasArea)
+			}
+		})
 	}
 }

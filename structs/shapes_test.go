4f5ba5e3ad9ea4171e43f0eb76ec784bfade2bd8
manujagobind <manujagobind@gmail.com>
2022-08-10 22:11:07 +0530

    Refactor

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -18,15 +18,15 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tc := range areaTests {
 		got := tc.shape.Area()
 		if got != tc.want {
-			t.Errorf("got %.2f, want %.2f", got, tc.want)
+			t.Errorf("%#v got %.2f, want %.2f", tc.shape, got, tc.want)
 		}
 	}
 }

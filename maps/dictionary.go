59e3a284ae20a0de0ba159fabeb0c7c47e6b92ef
manujagobind <manujagobind@gmail.com>
2022-08-14 16:21:41 +0530

    Refactor - implement the Error interface

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,12 +1,16 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
 
 func (d Dictionary) Search(word string) (string, error) {

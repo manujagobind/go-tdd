06750b4bc3cc32ac4b3291284a6eb6dad7664886
manujagobind <manujagobind@gmail.com>
2022-08-14 16:39:44 +0530

    Delete entry from map

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -73,6 +73,17 @@ func TestUpdate(t *testing.T) {
 	})
 }
 
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
+	}
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {

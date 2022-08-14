81fd30475ceecb2028d1e09422b894b740fc24f8
manujagobind <manujagobind@gmail.com>
2022-08-14 15:53:17 +0530

    Add to maps

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -22,6 +22,23 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {

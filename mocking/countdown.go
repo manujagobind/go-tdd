36313cee6f44ea677617322f15e0a1a7cc744c3e
manujagobind <manujagobind@gmail.com>
2022-08-15 15:46:12 +0530

    Mocking using dependency injection / interfaces

---
+++ mocking/countdown.go
@@ -0,0 +1,29 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}

3b4d3866970e0f4bdc43e4ae2a6de7c7a9d98920
manujagobind <manujagobind@gmail.com>
2022-08-15 17:44:17 +0530

    Use mock server from httptest

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,15 +1,33 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastUrl := "http://www.quii.dev"
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	want := fastUrl
-	got := Racer(slowURL, fastUrl)
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
 
 	if got != want {
 		t.Errorf("got %q, want %q, got, want", got, want)
 	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
